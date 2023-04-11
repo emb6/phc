e9a6080c8a556788e53d0a4bc325fa07bbd3d45d
Sandy <121004962+emb6@users.noreply.github.com>
2023-04-11 11:35:56 +0530

    added code

--- components/ProductDetails/ProductDetails.jsx
+++ components/ProductDetails/ProductDetails.jsx
@@ -1,22 +1,25 @@
-import React from "react";
+"use client";
+import React, { useState, useRef } from "react";
 import { Poppins } from "next/font/google";
+import Rupee from "../Icons/Rupee";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "600"] });
+
 export default function ProductDetailTab() {
   return (
     <>
-      <section className="py-20">
+      <section className="py-20 bg-white">
         <div className="max-w-7xl mx-auto px-4 py-5 ">
           <div className="flex  justify-center gap-10">
-            <div className="p-10 flex items-center bg-white ">
+            <div className="flex items-center shadow-lg rounded-md ">
               <img
-                src="https://themeim.com/demo/vaxin/demo-one/media/images/ingredient2/ing-one.png"
+                src="https://purablehealthcare.com/img/shop/img6.jpg"
                 alt=""
-                width={200}
+                width={587}
                 height="auto"
               />
             </div>
-            <div className={` ${poppins.className} `}>
+            <div className={` ${poppins.className} flex-1`}>
               <h1 className="font-semibold text-xl pb-2">
                 Vaxin Regular (500mg), Mild Intake Lorem Ipsum Dolor
               </h1>
@@ -74,6 +77,42 @@ export default function ProductDetailTab() {
                 <p className="ml-2 text-sm font-medium text-gray-500 dark:text-gray-400">
                   4.95 out of 5
                 </p>
+              </div>
+              <div className={` ${poppins.className} mt-5`}>
+                <div className="bg-gray-100 w-[100px] text-center p-2 my-3">
+                  <span className="font-semibold text-primary-purple">
+                    IN STOCK
+                  </span>
+                </div>
+                <div className="flex flex-start items-center gap-5">
+                  <span className="font-semibold text-4xl">₹ 12.00</span>
+                  <span className="text-red-600 line-through text-2xl">
+                    ₹ 12.00
+                  </span>
+                </div>
+              </div>
+              <p className="py-5 text-gray-500">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco.
+              </p>
+              <div className="text-gray-500">
+                <p className="pb-2 font-semibold">Benefits:</p>
+                <ui className="leading-[2rem] list-image-[url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTQiIGhlaWdodD0iMTIiIHZpZXdCb3g9IjAgMCAxNCAxMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiBmaWxsPSIjMzhiZGY4Ij48cGF0aCBmaWxsLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMy42ODUuMTUzYS43NTIuNzUyIDAgMCAxIC4xNDMgMS4wNTJsLTggMTAuNWEuNzUuNzUgMCAwIDEtMS4xMjcuMDc1bC00LjUtNC41YS43NS43NSAwIDAgMSAxLjA2LTEuMDZsMy44OTQgMy44OTMgNy40OC05LjgxN2EuNzUuNzUgMCAwIDEgMS4wNS0uMTQzWiIgLz48L3N2Zz4=)]">
+                  <li>
+                    High protein from quality whey, supports faster recovery
+                  </li>
+                  <li>
+                    Maintain positive N-Balance thus prevent loss of lean body
+                    mass & support anabolism
+                  </li>
+                  <li> Increases pharmacological response</li>
+                  <li>Contains BCAA's for faster tissue regeneration</li>
+                  <li>Provides Omega 3 & Omega 6 fatty acids</li>
+                  <li>Fulfils increased nutritional requirements</li>
+                  <li>Gluten free & preservative free</li>
+                  <li>Improves quality of life</li>
+                </ui>
               </div>
             </div>
           </div>
