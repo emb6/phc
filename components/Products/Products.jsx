c4f835449722afadc854ce12793c3d13181225d8
Sandy <121004962+emb6@users.noreply.github.com>
2023-04-14 12:49:19 +0530

    slider dynamic

--- components/Products/Products.jsx
+++ components/Products/Products.jsx
@@ -8,6 +8,33 @@ const poppins = Poppins({
   weight: ["100", "300", "400", "500", "600"],
 });
 
+const productData = [
+  {
+    image: "https://purablehealthcare.com/img/shop/img5.jpg",
+    name: "Puralbumen Powder",
+    description: "High protein from quality whey, supports faster recovery",
+    sale_price: "1399.00",
+    strike_price: "1599.00",
+    offer: "20% Off",
+  },
+  {
+    image: "https://purablehealthcare.com/img/shop/img6.jpg",
+    name: "Neofeed Powder",
+    description: "High protein from quality whey, supports faster recovery",
+    sale_price: "1199.00",
+    strike_price: "1499.00",
+    offer: "15% Off",
+  },
+  {
+    image: "https://purablehealthcare.com/img/shop/img7.jpg",
+    name: "Purwhey Powder",
+    description: "",
+    sale_price: "₹1199.00",
+    strike_price: "",
+    offer: "",
+  },
+];
+
 export default function AboutProduct() {
   const settings = {
     dots: true,
@@ -34,174 +61,36 @@ export default function AboutProduct() {
       <section className="px-5 md:py-10 lg:16 bg-secondary-gray">
         <div className="max-w-7xl mx-auto px-4 py-5 lg:px-10 lg:py-16">
           <Slider {...settings} className="lg:pb-16 ">
-            <div className="custom-slide">
-              <div class="mt-11 w-80 mr-1 transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
-                <img
-                  class="h-48 w-full object-cover object-center"
-                  src="https://images.unsplash.com/photo-1674296115670-8f0e92b1fddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                  alt="Product Image"
-                />
-                <div class="p-4">
-                  <h2 class="mb-2 text-lg font-medium dark:text-white text-gray-900">
-                    Product Name
-                  </h2>
-                  <p class="mb-2 text-base dark:text-gray-300 text-gray-700">
-                    Product description goes here.
-                  </p>
-                  <div class="flex items-center">
-                    <p class="mr-2 text-lg font-semibold text-gray-900 dark:text-white">
-                      $20.00
+            {productData.map((item, index) => (
+              <div className="custom-slide">
+                <div class="mt-11 w-80 mr-1 transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
+                  <img
+                    class="h-48 w-full object-cover object-center"
+                    src={item.image}
+                    alt="Product Image"
+                  />
+                  <div class="p-4">
+                    <h2 class="mb-2 text-lg font-medium dark:text-white text-gray-900">
+                      {item.name}
+                    </h2>
+                    <p class="mb-2 text-base dark:text-gray-300 text-gray-700">
+                      {item.description}
                     </p>
-                    <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">
-                      $25.00
-                    </p>
-                    <p class="ml-auto text-base font-medium text-green-500">
-                      20% off
-                    </p>
+                    <div class="flex items-center">
+                      <p class="mr-2 text-lg font-semibold text-gray-900 dark:text-white">
+                        {item.sale_price}
+                      </p>
+                      <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">
+                        {item.strike_price}
+                      </p>
+                      <p class="ml-auto text-base font-medium text-green-500">
+                        {item.offer}
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="custom-slide">
-              <div class="mt-11 w-80 mr-1 transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
-                <img
-                  class="h-48 w-full object-cover object-center"
-                  src="https://images.unsplash.com/photo-1674296115670-8f0e92b1fddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                  alt="Product Image"
-                />
-                <div class="p-4">
-                  <h2 class="mb-2 text-lg font-medium dark:text-white text-gray-900">
-                    Product Name
-                  </h2>
-                  <p class="mb-2 text-base dark:text-gray-300 text-gray-700">
-                    Product description goes here.
-                  </p>
-                  <div class="flex items-center">
-                    <p class="mr-2 text-lg font-semibold text-gray-900 dark:text-white">
-                      $20.00
-                    </p>
-                    <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">
-                      $25.00
-                    </p>
-                    <p class="ml-auto text-base font-medium text-green-500">
-                      20% off
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="custom-slide">
-              <div class="mt-11 w-80 mr-1 transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
-                <img
-                  class="h-48 w-full object-cover object-center"
-                  src="https://images.unsplash.com/photo-1674296115670-8f0e92b1fddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                  alt="Product Image"
-                />
-                <div class="p-4">
-                  <h2 class="mb-2 text-lg font-medium dark:text-white text-gray-900">
-                    Product Name
-                  </h2>
-                  <p class="mb-2 text-base dark:text-gray-300 text-gray-700">
-                    Product description goes here.
-                  </p>
-                  <div class="flex items-center">
-                    <p class="mr-2 text-lg font-semibold text-gray-900 dark:text-white">
-                      $20.00
-                    </p>
-                    <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">
-                      $25.00
-                    </p>
-                    <p class="ml-auto text-base font-medium text-green-500">
-                      20% off
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="custom-slide">
-              <div class="mt-11 w-80 mr-1 transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
-                <img
-                  class="h-48 w-full object-cover object-center"
-                  src="https://images.unsplash.com/photo-1674296115670-8f0e92b1fddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                  alt="Product Image"
-                />
-                <div class="p-4">
-                  <h2 class="mb-2 text-lg font-medium dark:text-white text-gray-900">
-                    Product Name
-                  </h2>
-                  <p class="mb-2 text-base dark:text-gray-300 text-gray-700">
-                    Product description goes here.
-                  </p>
-                  <div class="flex items-center">
-                    <p class="mr-2 text-lg font-semibold text-gray-900 dark:text-white">
-                      $20.00
-                    </p>
-                    <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">
-                      $25.00
-                    </p>
-                    <p class="ml-auto text-base font-medium text-green-500">
-                      20% off
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="custom-slide">
-              <div class="mt-11 w-80 mr-1 transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
-                <img
-                  class="h-48 w-full object-cover object-center"
-                  src="https://images.unsplash.com/photo-1674296115670-8f0e92b1fddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                  alt="Product Image"
-                />
-                <div class="p-4">
-                  <h2 class="mb-2 text-lg font-medium dark:text-white text-gray-900">
-                    Product Name
-                  </h2>
-                  <p class="mb-2 text-base dark:text-gray-300 text-gray-700">
-                    Product description goes here.
-                  </p>
-                  <div class="flex items-center">
-                    <p class="mr-2 text-lg font-semibold text-gray-900 dark:text-white">
-                      $20.00
-                    </p>
-                    <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">
-                      $25.00
-                    </p>
-                    <p class="ml-auto text-base font-medium text-green-500">
-                      20% off
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="custom-slide">
-              <div class="mt-11 w-80 mr-1 transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
-                <img
-                  class="h-48 w-full object-cover object-center"
-                  src="https://images.unsplash.com/photo-1674296115670-8f0e92b1fddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                  alt="Product Image"
-                />
-                <div class="p-4">
-                  <h2 class="mb-2 text-lg font-medium dark:text-white text-gray-900">
-                    Product Name
-                  </h2>
-                  <p class="mb-2 text-base dark:text-gray-300 text-gray-700">
-                    Product description goes here.
-                  </p>
-                  <div class="flex items-center">
-                    <p class="mr-2 text-lg font-semibold text-gray-900 dark:text-white">
-                      $20.00
-                    </p>
-                    <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">
-                      $25.00
-                    </p>
-                    <p class="ml-auto text-base font-medium text-green-500">
-                      20% off
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
+            ))}
           </Slider>
         </div>
       </section>
