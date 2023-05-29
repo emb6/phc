9bb9c0b0f8a630332fd0d180bba4db141a226c96
Sandy <121004962+emb6@users.noreply.github.com>
2023-05-29 19:30:36 +0530

    added link product page

--- components/Products/Products.jsx
+++ components/Products/Products.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Slider from "react-slick";
 import { Poppins } from "next/font/google";
+import Link from "next/link";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -16,6 +17,7 @@ const productData = [
     sale_price: "1399.00",
     strike_price: "1599.00",
     offer: "20% Off",
+    link: "/products/puralbumen",
   },
   {
     image: "https://purablehealthcare.com/img/shop/img6.jpg",
@@ -24,6 +26,7 @@ const productData = [
     sale_price: "1199.00",
     strike_price: "1499.00",
     offer: "15% Off",
+    link: "/products/neofeed",
   },
   {
     image: "https://purablehealthcare.com/img/shop/img7.jpg",
@@ -32,6 +35,7 @@ const productData = [
     sale_price: "₹1199.00",
     strike_price: "",
     offer: "5% Off",
+    link: "/products/purwhey",
   },
 ];
 
@@ -72,32 +76,34 @@ export default function AboutProduct() {
           <Slider {...settings} className="lg:pb-16 ">
             {productData.map((item, index) => (
               <div className="custom-slide" key={index}>
-                <div class="mt-11 w-80 mr-1 transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
-                  <img
-                    class="h-full w-full object-cover object-center"
-                    src={item.image}
-                    alt="Product Image"
-                  />
-                  <div class="p-4">
-                    <h2 class="mb-2 text-lg font-medium dark:text-white text-gray-900">
-                      {item.name}
-                    </h2>
-                    <p class="mb-2 text-base dark:text-gray-300 text-gray-700">
-                      {item.description}
-                    </p>
-                    <div class="flex items-center">
-                      <p class="mr-2 text-lg font-semibold text-gray-900 dark:text-white">
-                        {item.sale_price}
+                <Link href={item.link}>
+                  <div class="mt-11 w-80 mr-1 transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
+                    <img
+                      class="h-full w-full object-cover object-center"
+                      src={item.image}
+                      alt="Product Image"
+                    />
+                    <div class="p-4">
+                      <h2 class="mb-2 text-lg font-medium dark:text-white text-gray-900">
+                        {item.name}
+                      </h2>
+                      <p class="mb-2 text-base dark:text-gray-300 text-gray-700">
+                        {item.description}
                       </p>
-                      <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">
-                        {item.strike_price}
-                      </p>
-                      <p class="ml-auto text-base font-medium text-green-500">
-                        {item.offer}
-                      </p>
+                      <div class="flex items-center">
+                        <p class="mr-2 text-lg font-semibold text-gray-900 dark:text-white">
+                          {item.sale_price}
+                        </p>
+                        <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">
+                          {item.strike_price}
+                        </p>
+                        <p class="ml-auto text-base font-medium text-green-500">
+                          {item.offer}
+                        </p>
+                      </div>
                     </div>
                   </div>
-                </div>
+                </Link>
               </div>
             ))}
           </Slider>
